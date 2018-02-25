75b5edad0f5540229f8e5cccba40e0bb4c5cb57d
Iuliana Cosmina <iuliana.cosmina@gmail.com>
2018-02-25 18:03:01 +0200

    001 added first sources for chapter 3

---
+++ chapter03/src/main/java/com/apress/bgn/ch3/SimpleReader.java
@@ -0,0 +1,5 @@
+package com.apress.bgn.ch3;
+
+public class SimpleReader {
+
+}
---
+++ chapter03/src/main/java/com/apress/bgn/ch3/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Contains classes used for reading information from various sources.
+ * @since 1.0-SNAPSHOT
+ * @author iuliana.cosmina
+ * @version 1.0-SNAPSHOT
+ */
+package com.apress.bgn.ch3;
