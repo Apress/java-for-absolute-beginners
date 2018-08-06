fddd9ccbfe11b1c197f84c4577045052488b083b
iuliana <iuliana.cosmina@gmail.com>
2018-08-06 23:14:08 +0100

    026 added example with reading bytes

--- chapter10/src/main/java/com/apress/bgn/ch10/ReadingFormStdinDemo.java
+++ chapter10/src/main/java/com/apress/bgn/ch10/ReadingFormStdinDemo.java
@@ -10,7 +10,11 @@ public class ReadingFormStdinDemo {
     public static void main(String... args) throws IOException {
         System.out.print("Press any key to terminate:");
 
-        int read = System.in.read();
+        byte[] b = new byte[3];
+        int read = System.in.read(b);
+        for (int i = 0; i < b.length; ++i) {
+            System.out.println(b[i]);
+        }
         System.out.println("Key pressed: " + read);
     }
 }
