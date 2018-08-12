e558b4efa68579d2ad255d6907b1838d7c3d6185
iuliana <iuliana.cosmina@gmail.com>
2018-08-12 01:29:45 +0100

    027 added swing and awt examples

---
+++ chapter10/using-scanner/src/main/java/com/apress/bgn/ch10/ReadingFormStdinDemo.java
@@ -0,0 +1,139 @@
+/*
+Freeware License, some rights reserved
+
+Copyright (c) 2018 Iuliana Cosmina
+
+Permission is hereby granted, free of charge, to anyone obtaining a copy
+of this software and associated documentation files (the "Software"),
+to work with the Software within the limits of freeware distribution and fair use.
+This includes the rights to use, copy, and modify the Software for personal use.
+Users are also allowed and encouraged to submit corrections and modifications
+to the Software for the benefit of other users.
+
+It is not allowed to reuse,  modify, or redistribute the Software for
+commercial use in any way, or for a user's educational materials such as books
+or blog articles without prior permission from the copyright holder.
+
+The above copyright notice and this permission notice need to be included
+in all copies or substantial portions of the software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS OR APRESS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+package com.apress.bgn.ch10;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Created by iuliana.cosmina on 3/6/18
+ */
+public class ReadingFormStdinDemo {
+
+    public static final String EXIT = "exit";
+    public static final String HELP = "help";
+    public static final String BYTE = "byte";
+    public static final String SHORT = "short";
+    public static final String INT = "int";
+    public static final String BOOLEAN = "bool";
+    public static final String DOUBLE = "double";
+    public static final String LINE = "line";
+    public static final String BIGINT = "bigint";
+    public static final String TEXT = "text";
+    public static final String LONGS = "longs";
+
+    public static void main(String... args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        String help = getHelpString();
+        System.out.println(help);
+
+        String input;
+        do {
+            System.out.print("Enter option: ");
+            input = sc.nextLine();
+
+            switch (input) {
+                case HELP:
+                    System.out.println(help);
+                    break;
+                case EXIT:
+                    System.out.println("Hope you had fun. Buh-bye!");
+                    break;
+                case BYTE:
+                    byte b = sc.nextByte();
+                    System.out.println("Nice byte there: " + b);
+                    sc.nextLine();
+                    break;
+                case SHORT:
+                    short s = sc.nextShort();
+                    System.out.println("Nice short there: " + s);
+                    sc.nextLine();
+                    break;
+                case INT:
+                    int i = sc.nextInt();
+                    System.out.println("Nice int there: " + i);
+                    sc.nextLine();
+                    break;
+                case BOOLEAN:
+                    boolean bool = sc.nextBoolean();
+                    System.out.println("Nice boolean there: " + bool);
+                    sc.nextLine();
+                    break;
+                case DOUBLE:
+                    double d = sc.nextDouble();
+                    System.out.println("Nice double there: " + d);
+                    sc.nextLine();
+                    break;
+                case LINE:
+                    String line = sc.nextLine();
+                    System.out.println("Nice line of text there: " + line);
+                    break;
+                case BIGINT:
+                    BigInteger bi = sc.nextBigInteger();
+                    System.out.println("Nice big integer there: " + bi);
+                    sc.nextLine();
+                    break;
+                case TEXT:
+                    String text = sc.next();
+                    System.out.println("Nice text there: " + text);
+                    sc.nextLine();
+                    break;
+                case LONGS:
+                    List<Long> longList = new ArrayList<>();
+                    while (sc.hasNextLong()) {
+                        longList.add(sc.nextLong());
+                    }
+                    System.out.println("Nice long list there: " + longList);
+                    // else all done
+                    sc.nextLine();
+                    sc.nextLine();
+                    break;
+                default:
+                    System.out.println("No idea what you want bruh!");
+            }
+
+        } while (!input.equalsIgnoreCase(EXIT));
+    }
+
+    private static String getHelpString() {
+        return new StringBuilder("This application help you test various usage of Scanner. Enter type to be read next:")
+                .append("\n\t help >  displays this help")
+                .append("\n\t exit >  leave the application")
+                .append("\n\t byte > read a byte")
+                .append("\n\t short > read a short")
+                .append("\n\t int > read an int")
+                .append("\n\t bool > read a boolean")
+                .append("\n\t double > read a double")
+                .append("\n\t line > read a line of text")
+                .append("\n\t bigint > read a BigInteger")
+                .append("\n\t text > read a text value").toString();
+    }
+}
